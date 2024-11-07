e45c1b9b6d8ee4040390a78552ed5b3c6a29ba38
Felipe-M94 <felipe.silva.mendonca@gmail.com>
2024-11-07 17:07:38 -0300

    Ajuste na rota post, testes e integraçção com o BD.

--- fm_fastapi/app.py
+++ fm_fastapi/app.py
@@ -1,7 +1,11 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fm_fastapi.database import get_session
+from fm_fastapi.models import User
 from fm_fastapi.schemas import (
     Message,
     UserDB,
@@ -21,17 +25,37 @@ def read_root():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where((User.username == user.username) | (User.email == user.email))
+    )
 
-    database.append(user_with_id)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
-    return user_with_id
+    db_user = User(username=user.username, password=user.password, email=user.email)
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+def read_users(skip: int = 0, limit: int = 10, session: Session = Depends(get_session)):
+    user = session.scalars(select(User).offset(skip).limit(limit)).all()
+
+    return {'users': user}
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,22 +2,45 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fm_fastapi.app import app
-from fm_fastapi.models import table_registry
+from fm_fastapi.database import get_session
+from fm_fastapi.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
         yield session
 
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Felipe', email='felipe@teste.com', password='password')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
